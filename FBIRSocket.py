2b6b9f246281d733a447a61ce09cfeaa6efcd71f
Gabriel Ebanks <167536152+GEbanks@users.noreply.github.com>
2025-04-23 11:10:58 -0400

    Create FBIRSocket.py

---
+++ FBIRSocket.py
@@ -0,0 +1,70 @@
+import time
+import socketio
+import ir
+import threading
+from datetime import datetime
+# Chris was here
+
+# Create a Socket.IO client instance
+sio = socketio.Client()
+
+# Define connection event handler
+@sio.event
+def connect():
+    print("Connected to the server!")
+    sio.emit('getActiveClass')
+
+@sio.event
+def setClass(data):
+   print("Got class", data)
+   sio.emit('pollResp','True')
+
+@sio.event
+def disconnect():
+    print("Disconnected from the server!")
+
+@sio.event
+def irSignal():
+   print('Signal Received')
+
+
+def ir_loop():
+    try:
+        while True:
+            binary_signal = ir.getBinary()
+            hex_signal = ir.convertHex(binary_signal)
+
+            for name,code in ir.IRListener().buttons.items():
+                if hex_signal == hex(code):
+                    print(f"Button pressed: {name}")
+                    if name == "1":
+                        sio.emit('pollResp','Up')
+                    elif name == "2":
+                        sio.emit('pollResp','Wiggle')
+                    elif name == "3":
+                        sio.emit('pollResp','Down')
+                    elif name == "4":
+                        sio.emit('pollResp','remove')
+                    elif name == "play_pause":
+                        sio.emit('startPoll', ("3", False, "", [{"answer":"banana","weight":1,"color":"#ff0000"}, {"answer":"b","weight":1,"color":"#00ff00"}, {"answer":"c","weight":1,"color":"#0000ff"}], {}, 1, [], [], [], [], False))
+                        print('message sent')
+                    time.sleep(0.5)
+                    break
+    except KeyboardInterrupt:
+        print("Exiting program...")
+        GPIO.cleanup()
+        sio.disconnect()
+
+ir_thread = threading.Thread(target=ir_loop,daemon=True)
+ir_thread.start()
+
+extra_headers = {
+        "api": "13edef7790f96f0022ba1023040fc449da4d3cbb59ead5002524231599db72ea94f78d5e8ea6c54ffa52c38e5716fa444f9cd052332f7b0a554705fe52882692"
+}
+
+# Connect to the server
+sio.connect('http://172.16.3.219:420', headers=extra_headers)
+
+# Keep the client running to listen for events
+sio.wait()
+
