9d4435beabc1d1b9e80efacbb0dd4fe7e7311ae5
Gabriel Ebanks <167536152+GEbanks@users.noreply.github.com>
2025-04-23 11:35:15 -0400

    Update FBIRSocket.py

--- FBIRSocket.py
+++ FBIRSocket.py
@@ -1,3 +1,4 @@
+
 import time
 import socketio
 import ir
@@ -38,16 +39,15 @@ def ir_loop():
                 if hex_signal == hex(code):
                     print(f"Button pressed: {name}")
                     if name == "1":
-                        sio.emit('pollResp','Up')
+                        sio.emit('startPoll', ("3", False, "", [{"answer":"Up","weight":1,"color":"#00ff00"}, {"answer":"Wiggle","weight":1,"color":"#0000ff"}, {"answer":"Down","weight":1,"color":"#ff0000"}], {}, 1, [], [], [], [], False))
                     elif name == "2":
-                        sio.emit('pollResp','Wiggle')
+                        sio.emit('startPoll', ("2", False, "", [{"answer":"True","weight":1,"color":"#00ff00"}, {"answer":"False","weight":1,"color":"#ff0000"}], {}, 1, [], [], [], [], False))
                     elif name == "3":
-                        sio.emit('pollResp','Down')
+                        sio.emit('startPoll', ("1", False, "", [{"answer":"Done/ready?","weight":1,"color":"#00ff00"}], {}, 1, [], [], [], [], False))
                     elif name == "4":
-                        sio.emit('pollResp','remove')
+                        sio.emit('startPoll', ("4", False, "", [{"answer":"A","weight":1,"color":"#ff0000"}, {"answer":"B","weight":1,"color":"#00ff00"}, {"answer":"C","weight":1,"color":"#0000ff"}, {"answer":"D","weight":1,"color":"#A83295"}], {}, 1, [], [], [], [], False))
                     elif name == "play_pause":
-                        sio.emit('startPoll', ("3", False, "", [{"answer":"banana","weight":1,"color":"#ff0000"}, {"answer":"b","weight":1,"color":"#00ff00"}, {"answer":"c","weight":1,"color":"#0000ff"}], {}, 1, [], [], [], [], False))
-                        print('message sent')
+                        sio.emit('clearPoll')
                     time.sleep(0.5)
                     break
     except KeyboardInterrupt:
@@ -58,13 +58,13 @@ def ir_loop():
 ir_thread = threading.Thread(target=ir_loop,daemon=True)
 ir_thread.start()
 
+# Get teacher API key
 extra_headers = {
         "api": "13edef7790f96f0022ba1023040fc449da4d3cbb59ead5002524231599db72ea94f78d5e8ea6c54ffa52c38e5716fa444f9cd052332f7b0a554705fe52882692"
 }
 
-# Connect to the server
+# Set to formbar URL
 sio.connect('http://172.16.3.219:420', headers=extra_headers)
 
 # Keep the client running to listen for events
 sio.wait()
-
