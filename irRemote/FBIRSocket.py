043e557013cb47747c976cf1f0cc468aa4c35e74
Mr. Smith <csmith@ytech.edu>
2025-04-29 15:22:48 -0400

    Fixed some IR

--- irRemote/FBIRSocket.py
+++ irRemote/FBIRSocket.py
@@ -3,8 +3,7 @@ import time
 import socketio
 import ir
 import threading
-from datetime import datetime
-# Chris was here
+import config
 
 # Create a Socket.IO client instance
 sio = socketio.Client()
@@ -39,13 +38,15 @@ def ir_loop():
                 if hex_signal == hex(code):
                     print(f"Button pressed: {name}")
                     if name == "1":
-                        sio.emit('startPoll', ("3", False, "", [{"answer":"Up","weight":1,"color":"#00ff00"}, {"answer":"Wiggle","weight":1,"color":"#0000ff"}, {"answer":"Down","weight":1,"color":"#ff0000"}], {}, 1, [], [], [], [], False))
+                        sio.emit('startPoll', ("1", False, "", [{"answer":"Done/ready?","weight":1,"color":"#00ff00"}], False, 1, [], [], [], [], False))
                     elif name == "2":
-                        sio.emit('startPoll', ("2", False, "", [{"answer":"True","weight":1,"color":"#00ff00"}, {"answer":"False","weight":1,"color":"#ff0000"}], {}, 1, [], [], [], [], False))
+                        sio.emit('startPoll', ("2", False, "", [{"answer":"True","weight":1,"color":"#00ff00"}, {"answer":"False","weight":1,"color":"#ff0000"}], False, 1, [], [], [], [], False))
                     elif name == "3":
-                        sio.emit('startPoll', ("1", False, "", [{"answer":"Done/ready?","weight":1,"color":"#00ff00"}], {}, 1, [], [], [], [], False))
+                        sio.emit('startPoll', ("3", False, "", [{"answer":"Up","weight":1,"color":"#00ff00"}, {"answer":"Wiggle","weight":1,"color":"#0000ff"}, {"answer":"Down","weight":1,"color":"#ff0000"}], False, 1, [], [], [], [], False))
                     elif name == "4":
-                        sio.emit('startPoll', ("4", False, "", [{"answer":"A","weight":1,"color":"#ff0000"}, {"answer":"B","weight":1,"color":"#00ff00"}, {"answer":"C","weight":1,"color":"#0000ff"}, {"answer":"D","weight":1,"color":"#A83295"}], {}, 1, [], [], [], [], False))
+                        sio.emit('startPoll', ("4", False, "", [{"answer":"A","weight":1,"color":"#ff0000"}, {"answer":"B","weight":1,"color":"#00ff00"}, {"answer":"C","weight":1,"color":"#0000ff"}, {"answer":"D","weight":1,"color":"#A83295"}], False, 1, [], [], [], [], False))
+                    elif name == "5":
+                        sio.emit('startPoll', ("1", True, "", [{"answer":"Submit Text","weight":1,"color":"#ff0000"}], False, 1, [], [], [], [], False))
                     elif name == "play_pause":
                         sio.emit('clearPoll')
                     time.sleep(0.5)
@@ -60,11 +61,11 @@ ir_thread.start()
 
 # Get teacher API key
 extra_headers = {
-        "api": "13edef7790f96f0022ba1023040fc449da4d3cbb59ead5002524231599db72ea94f78d5e8ea6c54ffa52c38e5716fa444f9cd052332f7b0a554705fe52882692"
+        "api": config.api
 }
 
 # Set to formbar URL
-sio.connect('http://172.16.3.219:420', headers=extra_headers)
+sio.connect(config.formbar, headers=extra_headers)
 
 # Keep the client running to listen for events
 sio.wait()
---
+++ irRemote/config_template.py
@@ -0,0 +1,2 @@
+api = ""
+formbar = ""
